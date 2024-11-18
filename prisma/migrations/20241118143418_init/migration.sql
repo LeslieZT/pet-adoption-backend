82756d02bd348db71c4f2b5fdd76467e5359e2d7
LeslieZT <lfzarate30@gmail.com>
2024-11-18 09:35:24 -0500

    feat: add database model

---
+++ prisma/migrations/20241118143418_init/migration.sql
@@ -0,0 +1,230 @@
+-- CreateEnum
+CREATE TYPE "DonationType" AS ENUM ('ONE_TIME', 'SUBSCRIPTION');
+
+-- CreateEnum
+CREATE TYPE "ChannelType" AS ENUM ('ADOPTION', 'SHELTER');
+
+-- CreateTable
+CREATE TABLE "departments" (
+    "department_id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "departments_pkey" PRIMARY KEY ("department_id")
+);
+
+-- CreateTable
+CREATE TABLE "provinces" (
+    "province_id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "department_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "provinces_pkey" PRIMARY KEY ("province_id")
+);
+
+-- CreateTable
+CREATE TABLE "districts" (
+    "district_id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "province_id" INTEGER NOT NULL,
+    "department_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "districts_pkey" PRIMARY KEY ("district_id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "user_id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "channel" "ChannelType" NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT,
+    "birthdate" TIMESTAMP(3) NOT NULL,
+    "address" TEXT NOT NULL,
+    "district_id" INTEGER,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateTable
+CREATE TABLE "plans" (
+    "plan_id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "plans_pkey" PRIMARY KEY ("plan_id")
+);
+
+-- CreateTable
+CREATE TABLE "donations" (
+    "donation_id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "plan_id" INTEGER,
+    "user_id" UUID NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "type" "DonationType" NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "donations_pkey" PRIMARY KEY ("donation_id")
+);
+
+-- CreateTable
+CREATE TABLE "shelters" (
+    "shelter_id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "district_id" INTEGER NOT NULL,
+    "user_id" UUID,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "shelters_pkey" PRIMARY KEY ("shelter_id")
+);
+
+-- CreateTable
+CREATE TABLE "pet_categories" (
+    "pet_category_id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "pet_categories_pkey" PRIMARY KEY ("pet_category_id")
+);
+
+-- CreateTable
+CREATE TABLE "pet_breeds" (
+    "pet_breed_id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "category_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "pet_breeds_pkey" PRIMARY KEY ("pet_breed_id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "pet_id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "birthdate" TIMESTAMP(3) NOT NULL,
+    "weight" DECIMAL(6,2) NOT NULL,
+    "height" DECIMAL(6,2) NOT NULL,
+    "gender" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "behavior" TEXT[],
+    "profile_picture" TEXT,
+    "category_id" INTEGER NOT NULL,
+    "breed_id" INTEGER NOT NULL,
+    "shelter_id" UUID NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("pet_id")
+);
+
+-- CreateTable
+CREATE TABLE "pet_photos" (
+    "id" SERIAL NOT NULL,
+    "pet_id" INTEGER NOT NULL,
+    "url" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "pet_photos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "adoptions" (
+    "adoption_id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "application" JSONB NOT NULL,
+    "status" TEXT NOT NULL,
+    "pet_id" INTEGER NOT NULL,
+    "user_id" UUID NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "adoptions_pkey" PRIMARY KEY ("adoption_id")
+);
+
+-- CreateTable
+CREATE TABLE "favorite_pets" (
+    "favorite_pet_id" SERIAL NOT NULL,
+    "pet_id" INTEGER NOT NULL,
+    "user_id" UUID NOT NULL,
+    "value" BOOLEAN NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "favorite_pets_pkey" PRIMARY KEY ("favorite_pet_id")
+);
+
+-- AddForeignKey
+ALTER TABLE "provinces" ADD CONSTRAINT "provinces_department_id_fkey" FOREIGN KEY ("department_id") REFERENCES "departments"("department_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "districts" ADD CONSTRAINT "districts_province_id_fkey" FOREIGN KEY ("province_id") REFERENCES "provinces"("province_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "districts" ADD CONSTRAINT "districts_department_id_fkey" FOREIGN KEY ("department_id") REFERENCES "departments"("department_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_district_id_fkey" FOREIGN KEY ("district_id") REFERENCES "districts"("district_id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "donations" ADD CONSTRAINT "donations_plan_id_fkey" FOREIGN KEY ("plan_id") REFERENCES "plans"("plan_id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "donations" ADD CONSTRAINT "donations_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "shelters" ADD CONSTRAINT "shelters_district_id_fkey" FOREIGN KEY ("district_id") REFERENCES "districts"("district_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "shelters" ADD CONSTRAINT "shelters_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pet_breeds" ADD CONSTRAINT "pet_breeds_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "pet_categories"("pet_category_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "pet_categories"("pet_category_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_breed_id_fkey" FOREIGN KEY ("breed_id") REFERENCES "pet_breeds"("pet_breed_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_shelter_id_fkey" FOREIGN KEY ("shelter_id") REFERENCES "shelters"("shelter_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pet_photos" ADD CONSTRAINT "pet_photos_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("pet_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "adoptions" ADD CONSTRAINT "adoptions_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("pet_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "adoptions" ADD CONSTRAINT "adoptions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favorite_pets" ADD CONSTRAINT "favorite_pets_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("pet_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favorite_pets" ADD CONSTRAINT "favorite_pets_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE RESTRICT ON UPDATE CASCADE;
